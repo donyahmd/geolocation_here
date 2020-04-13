2367b4f5f86d35468adeb7286acb040de3d471e8
Doni Ahmad <donyahmd24@gmail.com>
2020-04-14 00:24:45 +0800

    customize space & user model

--- app/Space.php
+++ app/Space.php
@@ -4,7 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+use App\SpacePhoto;
+
 class Space extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function photos()
+    {
+        return $this->hasMany(SpacePhoto::class, 'space_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
