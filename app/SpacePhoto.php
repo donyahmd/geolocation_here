f9551ca4a13210dcac17794268ddc2c3b824f29f
Doni Ahmad <donyahmd24@gmail.com>
2020-04-14 00:35:21 +0800

    customize space photo model

--- app/SpacePhoto.php
+++ app/SpacePhoto.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Space;
+
 class SpacePhoto extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function space()
+    {
+        return $this->belongsTo(Space::class, 'space_id', 'id');
+    }
 }
