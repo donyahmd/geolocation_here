02c3b3f496690fcbbc5e9a415d9907bc7c39d229
Doni Ahmad <donyahmd24@gmail.com>
2020-04-14 16:49:50 +0800

    add Haversine Formula

--- app/Space.php
+++ app/Space.php
@@ -20,4 +20,21 @@ class Space extends Model
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
+
+    public function getSpaces($latitude, $longitude, $radius)
+    {
+        return $this->select('spaces.*')
+            ->selectRaw(
+                '( 6371 *
+                    acos( cos( radians(?) ) *
+                        cos( radians( latitude ) ) *
+                        cos( radians( longitude ) - radians(?) ) +
+                        sin( radians(?) ) *
+                        sin( radians( latitude ) )
+                    )
+                ) AS distance', [$latitude, $longitude, $latitude]
+            )
+            ->havingRaw('distance < ?', [$radius])
+            ->orderBy('distance', 'ASC');
+    }
 }
